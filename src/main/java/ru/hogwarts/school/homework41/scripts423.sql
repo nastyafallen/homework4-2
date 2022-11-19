e8376152f7ca6eb05166f3c23426ec90b2d6672f
Anastasia <102392534+nastyafallen@users.noreply.github.com>
2022-11-20 00:53:23 +0300

    Delete scripts423.sql

--- src/main/java/ru/hogwarts/school/homework41/scripts423.sql
+++
@@ -1,4 +0,0 @@
-SELECT student.name, student.age, faculty.name
-FROM student LEFT JOIN faculty ON faculty.id = student.faculty_id;
-
-SELECT s.name, s.age FROM student s INNER JOIN avatar a on s.id = a.student_id;
